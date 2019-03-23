8560ac8202c0a38684dda0e3777e766292a1269c
Stephanie Hutchinson <stephehutch@gmail.com>
2019-03-23 16:26:59 -0700

    added forms to the pages. They are not models.. yet

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import CheckIn from "../components/CheckIn";
+import UpdateUser from "../components/UpdateUser";
+import CreateGadaBot from "../components/CreateGadaBot";
+import {
+  Container
+ } from 'reactstrap';
+
 
 class UserProfile extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>
-                <p>User Profile</p>
-            </div>
-         );
+    
+
+    render() {
+      return (
+        <Container >
+         <p>Some text</p>
+         < CheckIn />
+         <UpdateUser />
+         <CreateGadaBot />
+         </Container>
+
+      );
     }
-}
+  }
+  
  
 export default UserProfile;
