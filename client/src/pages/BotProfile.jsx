3a3bbce176c4bbeca0d4b3f1def4b2674e9b6b6e
stephehutch <42480273+stephehutch@users.noreply.github.com>
2019-03-26 20:18:23 -0700

    Update BotProfile.jsx
    adding changes one by one to be sure nothing brakes

--- client/src/pages/BotProfile.jsx
+++ client/src/pages/BotProfile.jsx
@@ -1,23 +1,77 @@
 import React, { Component } from 'react';
-import CheckIn from "../components/CheckIn";
-import UpdateUser from "../components/UpdateUser";
-import CreateGadaBot from "../components/CreateGadaBot";
-import {
-  Container
-  } from 'reactstrap';  
-  
-  class BotProfile extends Component {
-    render() {
-      return (
-          <Container>
-            <p>Some text</p>
-           < CheckIn />
-           <UpdateUser />
-           <CreateGadaBot />
-       
-        </Container>
-      );
-    }
+//import './createGadaBot.css';
+import CheckIn from './components/checkIn';
+import UpdateUser from './components/updateUser'
+import CreateGadaBot from './components/createGadaBot'
+import testsBots from "./testBots.json";
+//import UpdateUser from './components/modals/updateUser'
+
+class App extends Component {
+
+state = {
+  // will match pot with the botId here
+  bot: testsBots,
+ };
+
+handleFactShow() {
+  //  setState({ 
+  //     showFact: true
+  // })
+  alert("the value of show ifact is: ")
+}
+
+  render() {
+    return (
+      <div className="container">
+        < CheckIn />
+        <br />
+     {this.state.bot.map(bot => (
+      <div alt="becauseItGotMad">
+              <div className="card" key={bot.id}>
+               <div className="row">
+                  <div className="col-3">
+                    <img className="card-img-top" src={bot.checkIns[0].pic}
+                      alt={bot.name} />
+                  </div>
+                  
+               <div class="card-body col-9">
+                <div className="card-title"> <h1>{bot.name}</h1></div>
+                <p><strong>Hometown: </strong>{bot.checkIns[0].location} </p>
+                <p><strong>Created Date: </strong>{bot.checkIns[0].date} </p>
+                </div>
+                </div>
+                </div>
+                <ul class="list-group">
+               {bot.checkIns.map(checkIn=> (
+
+              <li class="list-group-item">
+           
+                <div className="row">
+                  <div className="col-3">
+                    <img className="card-img-top" src={checkIn.pic}
+                      alt={checkIn.location} />
+                  </div>
+                  <div className="col-9">
+                    <div className="card-body">
+                      <h4>{checkIn.location}</h4>
+                      <p className="card-text"> {checkIn.journalEntry}</p>
+                      <small>{checkIn.date}</small>
+                    </div>
+                  </div>
+                
+                </div>
+            
+             
+            </li>
+        
+            ))}
+            </ul>
+     
+        </div>
+          ))}
+      </div>
+    );
   }
-  
-  export default BotProfile;
+}
+
+export default App;
